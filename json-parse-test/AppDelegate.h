b9ee95e8a1c410861ef1ac7841ce3969209e41a2
Alex Rudyak <al.rudyak@gmail.com>
2015-08-07 14:38:36 +0300

    Initial Commit

---
+++ json-parse-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  json-parse-test
+//
+//  Created by Alex Rudyak on 8/7/15.
+//  Copyright (c) 2015 *instinctools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
